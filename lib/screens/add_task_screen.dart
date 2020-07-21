673282266dd11e02520c3e29ee54080d3c01f375
vishal-s-k <50524633+vishal-s-k@users.noreply.github.com>
2020-07-21 15:46:56 +0530

    Update add_task_screen.dart

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todoey_flutter/models/task.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey_flutter/models/task_data.dart';
 
